d837c8b5e84e37dbacce25a112c5bbbdbab4ff1f
SergeyKarasyov <sergeyvkarasyov@gmail.com>
2020-10-04 01:47:08 +0200

    implementation of levels in logger

--- source/ILogger.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-class ILogger {
-public:
-  virtual ~ILogger(){};
-  virtual void log(const std::string &data) const = 0;
-};
--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,15 @@
 #include "Logger.hpp"
 
 int main() {
-  const auto logger = Logger::instance();
+  auto logger = Logger::instance();
   logger.log("HELLO WORLD");
+  logger.log("HELLO WORLD INFO", ILogger::LogLevel::INFO);
+  logger.log("HELLO WORLD WARNING", ILogger::LogLevel::WARNING);
+  logger.log("HELLO WORLD ERROR", ILogger::LogLevel::ERROR);
+  logger.log("HELLO WORLD NEVER_SKIP", ILogger::LogLevel::NEVER_SKIP);
+  logger.setLogLevel(ILogger::LogLevel::ERROR);
+  logger.log("HELLO WORLD INFO while level is ERROR", ILogger::LogLevel::INFO);
+  logger.log("HELLO WORLD WARNING while level is ERROR", ILogger::LogLevel::WARNING);
+  logger.log("HELLO WORLD ERROR while level is ERROR", ILogger::LogLevel::ERROR);
+  logger.log("HELLO WORLD NEVER_SKIP while level is ERROR", ILogger::LogLevel::NEVER_SKIP);
 }
